3867fd723da8a8e7499cb6b8c1c8e980115014b2
tommyliu313 <liuchaktin@gmail.com>
2022-09-16 00:41:20 +0800

    20220916 0041

--- formsignin.php
+++ formsignin.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--compulsorypartstart-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap-5.2.1-dist/css/bootstrap.min.css"></link>
+    <script src="./css/bootstrap-5.2.1-dist/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<!--compulsorypartend-->
 <!--PasswordValidation-->
+
+<!--resetbutton-->
+
+<!--submitbutton-->
