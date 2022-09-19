9375948e34457566fe8fb8bbbe2b15cb2f6e4730
tommyliu313 <liuchaktin@gmail.com>
2022-09-20 00:18:37 +0800

    20220920 0018

--- testing/apimethodweb.php
+++ testing/apimethodweb.php
@@ -1,5 +1,20 @@
 <?php
 $apimethod = array("get","post","put","delete");
+echo "Give an API method:";
+$requestapimethod = trim(fgets(STDIN,1024));
 $url = "localhost";
-echo $url . http_build_query($apimethod);
+if($requestapimethod == $apimethod[0]){
+    $obj = (object)$apimethod[0];
+    echo $url . http_build_query($obj);
+}elseif($requestapimethod == $apimethod[1]){
+    $obj = (object)$apimethod[1];
+    echo $url . http_build_query($obj);
+}elseif($requestapimethod == $apimethod[2]){
+    $obj = (object)$apimethod[2];
+    echo $url . http_build_query($obj);
+}elseif($requestapimethod == $apimethod[3]){
+    $obj = (object)$apimethod[3];
+    echo $url . http_build_query($obj);
+}
+
 ?>
