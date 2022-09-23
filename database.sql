199380bf10fbda12e4ce88abab56186b96ef14a5
tommyliu313 <liuchaktin@gmail.com>
2022-09-23 23:27:13 +0800

    20220923 2327

---
+++ database.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `MarketInformation.detail`(
+    marketid int(10) NOT NULL AUTO_INCREMENT,
+     timemod int(10) NOT NULL UNIQUE,
+     telone int(8) NOT NULL UNIQUE,
+     teltwo int(8) NOT NULL UNIQUE,
+    locationlocatid int(6) NOT NULL,
+    marketname char(20) NOT NULL UNIQUE,
+    PRIMARY KEY (marketid));
+
+
+CREATE TABLE location (
+ locatid int(6) NOT NULL AUTO_INCREMENT,
+ coordlat float NOT NULL UNIQUE,
+ coordlon float NOT NULL UNIQUE,
+ Regionregid smallint(5) NOT NULL,
+ Districtdistrictid int(10) NOT NULL,
+ addressname char(20),
+ PRIMARY KEY (locatid));
+
+CREATE TABLE businesstime (
+ busintid int(10),
+ starttmtime time(7),
+ endmtime timestamp NULL,
+ utc int(10));
+
+CREATE TABLE Region (
+ regid smallint(5) NOT NULL AUTO_INCREMENT,
+ regioneng int(10) NOT NULL,
+ PRIMARY KEY (regid));
+
+CREATE TABLE district (
+ districtid int(10) NOT NULL AUTO_INCREMENT,
+ districteng int(10),
+ PRIMARY KEY (districtid));
--- testing/updatefile.php
+++ testing/updatefile.php
@@ -1,12 +1,15 @@
 <?php
 
+//接收傳過來的東西
 $test = $_POST["test"];
 
+//數據庫設定
 $servername = 'localhost';
 $databaseuser = 'root';
 $datapassword = '';
 $databasename = 'test';
 
+//建立連結
 $connection = new mysqli($servername, $databaseuser, $datapassword, $databasename);
 
 $querysql = 'SELECT * FROM test';
@@ -14,7 +17,8 @@ $querysql = 'SELECT * FROM test';
 if($result = $connection->query($querysql)){
 
     $resultarr = array();
-    
+
+    $resultarr['test'] = $
 }
 
 
