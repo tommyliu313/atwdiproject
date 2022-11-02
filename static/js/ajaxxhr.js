5e0e4d11b60db6ab84d76533638f5cf59cdfa85a
tommyliu313 <liuchaktin@gmail.com>
2022-11-02 21:52:28 +0800

    20221102 2152

---
+++ static/js/ajaxxhr.js
@@ -0,0 +1,21 @@
+function action(){
+
+    var XHR = createXMLHttpRequest();
+    XHR.open("GET","../../../restfulapi/apimethodservice.php")
+}
+
+function search(){
+
+
+}
+
+function eliminate(){
+
+    var XHR = createXMLHttpRequest();
+    XHR.open("POST","../../../restfulapi/apimethodservice.php")
+}
+
+
+function (){
+    
+}
