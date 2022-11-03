c0d4aa2c40b58a3ea1ec86c94cbe09894625f405
tommyliu313 <liuchaktin@gmail.com>
2022-11-04 02:28:02 +0800

    20221104 0227

--- static/js/ajaxxhr.js
+++ static/js/ajaxxhr.js
@@ -1,21 +1,45 @@
 function action(){
 
     var XHR = createXMLHttpRequest();
-    XHR.open("GET","../../../restfulapi/apimethodservice.php")
+    XHR.open("GET","../../../restfulapi/apimethodservice.php");
+    XHR.onreadystatechange = function(){
+
+    };
 }
 
-function search(){
-
-
-}
-
-function eliminate(){
+function modify(){
 
     var XHR = createXMLHttpRequest();
-    XHR.open("POST","../../../restfulapi/apimethodservice.php")
+    XHR.open("PUT","../../../restfulapi/apimethodservice.php");
+    XHR.onreadystatechange = function(){
+
+    };
+
+
+}
+
+function remove(){
+
+    var XHR = createXMLHttpRequest();
+    XHR.open("DELETE","../../../restfulapi/apimethodservice.php");
+    XHR.onreadystatechange = function(){
+
+    };
 }
 
 
-function (){
+function insert(){
+    var 
+    var XHR = createXMLHttpRequest();
+    XHR.open("POST","../../../restfulapi/apimethodservice.php");
+    XHR.onreadystatechange = function(){
+
+    };
     
 }
+
+var deletebutton = document.getElementById("delete");
+var updatebutton = document.getElementById("update");
+
+deletebutton.addEventListener("click",remove);
+updatebutton.addEventListener("click",modify);
