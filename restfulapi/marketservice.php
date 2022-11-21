f5fdb3fd29875be456852d820346e50e709d6d35
Liu Chak Tin (227212713) <227212713@stu.vtc.edu.hk>
2022-11-21 17:43:50 +0800

    20221121 1743

--- restfulapi/marketservice.php
+++ restfulapi/marketservice.php
@@ -3,7 +3,7 @@
 class marketservice{
     
     function restserviceGET($param){
-        echo "restGET function is called";
+        echo "restserviceGET is triggered";
         echo "<br>";
         echo "You have reached the service";
 
@@ -102,6 +102,58 @@ class marketservice{
  
  
 }       
+  
+
+
+
+}
+    function restserviceDELETE($param){
+        echo "restserviceDELETE  is triggered";
+        echo "<br>";
+        echo "You have reached the service";
+
+        print_r($param);
+
+        require_once '../database/data/dbsetting.php';
+
+        $genre = array_shift($param);
+    
+        //Search the market by MarketID
+        if($genre ==='marketname'){
+
+            $marketname = array_shift($param);
+            
+            if(!isset($marketname) && !is_string($marketname)){
+                http_response_code(400);
+                $error = array();
+                $error['code'] = '1990';
+                $error['message'] ='missing parameter';
+                echo json_encode($error);
+            }
+            else{
+                $sql = "DELETE * FROM market WHERE marketname = '$marketname'";
+       
+            try{
+                $result = $connection->query($sql);
+                while($row = mysqli_fetch_object($result)){
+                    http_response_code(200);
+                    $content = array();
+                    $content = "You have deleted";
+                    $content['marketname'] = $row->marketname;
+                    $content .= "record";
+                }   
+                echo json_encode($content);
+            }
+            catch(Exception $e){
+                var_dump(http_response_code(404));
+                $output = array();
+                $output['status'] = 'error';
+                $output['code'] = '1991';
+                $output['message'] = 'tedt';
+            }}
+}   
+
+    //Search the market by district
     if($genre === 'district'){
 
         $district = array_shift($param);
@@ -115,10 +167,9 @@ class marketservice{
         }
         else{
             $sql = "SELECT DISTINCT districtname, marketname FROM market WHERE districtname = '$district'";
-        
+       
         try{
             $result = $connection->query($sql);
-
             while($row = mysqli_fetch_object($result)){
                 http_response_code(200);
                 $content = array();
@@ -126,11 +177,10 @@ class marketservice{
                 $content['marketname'] = $row->marketname;
                 echo json_encode($content);
             }
-
+            
         }
         //Error Handling
         catch(Exception $e){
-
             var_dump(http_response_code(404));
             $output = array();
             $output['status'] = 'error';
@@ -140,7 +190,50 @@ class marketservice{
 
     }
     if(empty($genre)){
+        http_response_code(424);
+        $error = array();
+        $error['code'] = "1990";
+        $error["message"] = "Invalid Input";
+        echo json_encode($error);
+    }
 
+
+}       
+    if($genre === 'district'){
+        $district = array_shift($param);
+
+    if(!isset($district)){
+        http_response_code(400);
+        $error = array();
+        $error['code'] = '1990';
+        $error['message'] ='missing parameter';
+        echo json_encode($error);
+    }
+    else{
+        $sql = "SELECT DISTINCT districtname, marketname FROM market WHERE districtname = '$district'";
+
+    try{
+        $result = $connection->query($sql);
+        while($row = mysqli_fetch_object($result)){
+            http_response_code(200);
+            $content = array();
+            $content['districtname'] = $row->districtname;
+            $content['marketname'] = $row->marketname;
+            echo json_encode($content);
+        }
+
+    }
+    //Error Handling
+    catch(Exception $e){
+        var_dump(http_response_code(404));
+        $output = array();
+        $output['status'] = 'error';
+        $output['code'] = '1991';
+        $output['message'] = 'SQL execution failure';
+    }
+
+    }
+    if(empty($genre)){
         http_response_code(424);
         $error = array();
         $error['code'] = "1990";
@@ -153,8 +246,9 @@ class marketservice{
 
 
 
-}
-    function restserviceDELETE($param){
+    }
+
+    function restservicePUT($param){
         echo "restGET function is called";
         echo "<br>";
         echo "You have reached the service";
@@ -221,6 +315,154 @@ class marketservice{
         }
         else{
             $sql = "SELECT DISTINCT districtname, marketname FROM market WHERE districtname = '$district'";
+       
+        try{
+            $result = $connection->query($sql);
+            while($row = mysqli_fetch_object($result)){
+                http_response_code(200);
+                $content = array();
+                $content['districtname'] = $row->districtname;
+                $content['marketname'] = $row->marketname;
+                echo json_encode($content);
+            }
+            
+        }
+        //Error Handling
+        catch(Exception $e){
+            var_dump(http_response_code(404));
+            $output = array();
+            $output['status'] = 'error';
+            $output['code'] = '1991';
+            $output['message'] = 'SQL execution failure';
+        }
+
+    }
+    if(empty($genre)){
+        http_response_code(424);
+        $error = array();
+        $error['code'] = "1990";
+        $error["message"] = "Invalid Input";
+        echo json_encode($error);
+    }
+
+
+}       
+    if($genre === 'district'){
+        $district = array_shift($param);
+
+    if(!isset($district)){
+        http_response_code(400);
+        $error = array();
+        $error['code'] = '1990';
+        $error['message'] ='missing parameter';
+        echo json_encode($error);
+    }
+    else{
+        $sql = "SELECT DISTINCT districtname, marketname FROM market WHERE districtname = '$district'";
+
+    try{
+        $result = $connection->query($sql);
+        while($row = mysqli_fetch_object($result)){
+            http_response_code(200);
+            $content = array();
+            $content['districtname'] = $row->districtname;
+            $content['marketname'] = $row->marketname;
+            echo json_encode($content);
+        }
+
+    }
+    //Error Handling
+    catch(Exception $e){
+        var_dump(http_response_code(404));
+        $output = array();
+        $output['status'] = 'error';
+        $output['code'] = '1991';
+        $output['message'] = 'SQL execution failure';
+    }
+
+    }
+    if(empty($genre)){
+        http_response_code(424);
+        $error = array();
+        $error['code'] = "1990";
+        $error["message"] = "Invalid Input";
+        echo json_encode($error);
+    }
+
+
+}
+
+
+
+    }
+    
+    function restservicePOST($param){
+        echo "restGET function is called";
+        echo "<br>";
+        echo "You have reached the service";
+
+        print_r($param);
+
+        require_once '../database/data/dbsetting.php';
+
+        $genre = array_shift($param);
+    
+        //Search the market by MarketID
+        if($genre ==='marketId'){
+
+            $marketId = array_shift($param);
+            
+            if(!isset($marketId) && !is_numeric($marketId)){
+                http_response_code(400);
+                $error = array();
+                $error['code'] = '1990';
+                $error['message'] ='missing parameter';
+                echo json_encode($error);
+            }
+            else{
+                $sql = "SELECT * FROM market WHERE marketId = '$marketId'";
+       
+            try{
+                $result = $connection->query($sql);
+                while($row = mysqli_fetch_object($result)){
+                    http_response_code(200);
+                    $content = array();
+                    $content['marketId'] = $row->marketId;
+                    $content['marketname'] = $row->marketname;
+                    $content['regionname'] = $row->regionname;
+                    $content['districtname'] = $row->districtname;
+                    $content['address']=$row->address;
+                    $content['contact']['contact1']= $row->contact1;
+                    $content['contact']['contact2']= $row->contact2;
+                    $content['openinghour']=$row->openinghour;
+                    $content['tenancycomd']=$row->tenancycomd;
+                    $content['nosstall']=$row->nosstall;
+                }   
+                echo json_encode($content);
+            }
+            catch(Exception $e){
+                var_dump(http_response_code(404));
+                $output = array();
+                $output['status'] = 'error';
+                $output['code'] = '1991';
+                $output['message'] = 'SQL execution failure';
+            }}
+}   
+
+    //Search the market by district
+    if($genre === 'district'){
+
+        $district = array_shift($param);
+
+        if(!isset($district)){
+            http_response_code(400);
+            $error = array();
+            $error['code'] = '1990';
+            $error['message'] ='missing parameter';
+            echo json_encode($error);
+        }
+        else{
+            $sql = " FROM market WHERE districtname = '$district'";
        
         try{
             $result = $connection->query($sql);
